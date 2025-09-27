f3d52404d791da362bf46ac408be6a40551a9589
Josh Hong <joshsw@umich.edu>
2025-09-27 13:45:05 -0400

    Initialize front and back end work enviro

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>FullStack Fitness</h1>
+      </header>
+      <main>
+        {/* Your form components and results display will go here */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
